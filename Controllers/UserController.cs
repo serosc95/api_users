1d48fa6fce86dda1412083ea6280f4823a5b4053
serosc95 <oscardleon95@gmail.com>
2023-07-25 00:32:04 -0500

    models and controller finished

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_users.Controllers;
+
+public class UserController : Controller
+{
+    private readonly HttpClient _httpClient;
+    public UserController()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("URL API");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var response =  await _httpClient.GetAsync("/users");
+        response.EnsureSuccessStatusCode();
+
+        var users  = await response.Content.ReadFromJsonAsync<List<UserModel>>();
+        return View(users);
+    }
+
+    [HttpGet]
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(UserModel user)
+    {
+        var response =  await _httpClient.PostAsJsonAsync("/users", user);
+        response.EnsureSuccessStatusCode();
+
+        return RedirectToAction("GetAllUsers");
+    }
+}
